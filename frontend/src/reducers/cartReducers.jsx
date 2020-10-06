87fcb62e1ee8cfe7dd67135bc1d0ed6a5791a070
lethanhnhan-dev <thanhnhanle1198@gmail.com>
2020-10-06 12:32:32 +0700

    Cart Screen [2]

--- frontend/src/reducers/cartReducers.jsx
+++ frontend/src/reducers/cartReducers.jsx
@@ -22,7 +22,13 @@ export const cartReducers = (state = { cartItems: [] }, action) => {
 					cartItems: [...state.cartItems, item],
 				};
 			}
-
+		case cartTypes.CART_REMOVE_ITEM:
+			return {
+				...state,
+				cartItems: state.cartItems.filter(
+					(x) => x.product !== action.payload,
+				),
+			};
 		default:
 			return state;
 	}
--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -11,7 +11,7 @@ import {
 	Button,
 	Card,
 } from "react-bootstrap";
-import { addToCart } from "../actions/cartActions";
+import { addToCart, removeFromCart } from "../actions/cartActions";
 
 const CartScreen = ({ match, location, history }) => {
 	const productId = match.params.id;
@@ -29,7 +29,7 @@ const CartScreen = ({ match, location, history }) => {
 	}, [dispatch, productId, qty]);
 
 	const removwFromCartHandler = (id) => {
-		console.log(id);
+		dispatch(removeFromCart(id));
 	};
 
 	const checkoutHandler = () => {
