9817683d3d3a6c5824eead2b24b09d95b6df2a8c
lethanhnhan-dev <thanhnhanle1198@gmail.com>
2020-10-09 23:51:39 +0700

    Place Order Screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import CartScreen from "./screens/CartScreen";
 import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
 import PaymentScreen from "./screens/PaymentScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import ProductScreen from "./screens/ProductScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 import RegisterScreen from "./screens/RegisterScreen";
@@ -21,6 +22,7 @@ const App = () => {
 					<Switch>
 						<Route path="/shipping" component={ShippingScreen} />
 						<Route path="/payment" component={PaymentScreen} />
+						<Route path="/placeorder" component={PlaceOrderScreen} />
 						<Route path="/login" component={LoginScreen} />
 						<Route path="/register" component={RegisterScreen} />
 						<Route path="/profile" component={ProfileScreen} />
